04e47707547b715772b8a1eddaf13af552bf3376
Vinicios Santana <vinicioss1592@gmail.com>
2023-03-28 13:27:37 -0300

    middleware tests

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,18 @@
-import { NextResponse, NextRequest } from 'next/server';
-import { getSession } from 'next-auth/react';
-import { redirect } from 'next/dist/server/api-utils';
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
+export async function middleware(req) {
 
-export async function middleware(req: NextRequest, res: NextResponse, next: () => void) {
-    const session = await getSession({ req });
-  
-    if (!session) {
-        redirect(res, '/login');
-    } else {
-      next();
-    }
-  }
-
-
-
-
-// import { getToken } from "next-auth/jwt";
-// import { NextResponse } from "next/server";
-
-// export async function middleware(req) {
-
-//     const token = await getToken({ req, secret: process.env.JWT_SECRET });
+    const token = await getToken({ req, secret: process.env.JWT_SECRET });
     
-//     const { pathname, origin } = req.nextUrl;
+    const { pathname, origin } = req.nextUrl;
 
-//     if (pathname.includes('/api/auth') || token) {
-//         return NextResponse.next();
-//     }
+    if (pathname.includes('/api/auth') || token) {
+        return NextResponse.next();
+    }
 
-//     if (!token && pathname !== '/login') {
-//         console.log(pathname)
-//         return NextResponse.redirect(`/login`);
-//     }
-// }
+    if (!token && pathname !== '/login') {
+        console.log(req.url)
+        return NextResponse.redirect(`/login`, req.url);
+    }
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,13 @@
 const theme = {
-  colors: {
-    background: '#040306',
-    lightbackground: '#1a1a1a',
-    bachgroundHighLight: '#292929',
-    text: '#9b9b9b',
-    textLight: '#ffffff',
-    primary: '#1db954',
-    primaryLight: '#58d685'
-  }
-};
-
-export default theme;
+    colors: {
+      background: '#040306',
+      lightbackground: '#1a1a1a',
+      bachgroundHighLight: '#292929',
+      text: '#9b9b9b',
+      textLight: '#ffffff',
+      primary: '#1db954',
+      primaryLight: '#58d685'
+    }
+  };
+  
+  export default theme;
